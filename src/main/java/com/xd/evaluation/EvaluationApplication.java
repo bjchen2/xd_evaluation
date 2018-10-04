3f5dcab88ab8949f46326db8532e9768edc642c0
bjchen2 <1210279050@qq.com>
2018-10-04 10:23:57 +0800

    add test

---
+++ src/main/java/com/xd/evaluation/EvaluationApplication.java
@@ -0,0 +1,12 @@
+package com.xd.evaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvaluationApplication.class, args);
+    }
+}
